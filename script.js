6f0e4509ad045730e8ccb3c5a3540f4a202f04d9
Élaff Ramos <valelaff10@gmail.com>
2024-10-30 10:23:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-function api(){
-
-    const user = document.getElementById('user').value
+function git() {
+    const user = document.getElementById('user').value;
     const resultado = document.querySelector('.resultado')
 
-    resultado.innerHTML = ''
+    resultado.innerHTML = '' // Limpa o resultado anterior
+
+    // Verifica se o usuário foi fornecido
+    if (!user) {
+        resultado.innerHTML = '<br> Digite algum nome de usuário!'
+        return;
+    }
 
     fetch(`https://api.github.com/users/${user}/repos`)
         .then(async res => {
-
+            if (!res.ok) {
+                throw new Error('Usuário não encontrado')
+            }
             let data = await res.json()
 
-            data.map(item => {
+            // Cria uma string para armazenar todos os repositórios
+            let repositorios = ''
 
-                resultado.innerHTML = `<br> <strong>${ item.name }</strong>`
-
+            data.forEach(item => {
+                repositorios += `  
+                    <div class="repositorio">
+                        Repositório: <a href="${item.html_url}" target="_blank"><b>${item.name}</b><br></a>
+                    </div>
+                `
             })
+
+            resultado.innerHTML = repositorios; // Exibe todos os repositórios de uma vez
+            
         })
         .catch(e => {
-            if(nome == 0){
-                resultado.innerHTML = '<br> Digite algum nome de usúsario!'
-            } else {
-                resultado.innerHTML = `<br> O usúario <strong>${user}</strong>, não foi encontrado`
-            }
-            
+            resultado.innerHTML = `<br> O usuário <strong>${user}</strong>, não foi encontrado.`
         })
 }
